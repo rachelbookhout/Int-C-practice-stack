f1edcf0ac3d1ff4d1c82bb2147f29df67a07ccbb
rachelbookhout <rbookhou@gmail.com>
2015-08-02 21:40:18 -0400

    first commit for stack problem

---
+++ Stack/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stack
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
---
+++ Stack/Stack.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+namespace Stack
+{
+	public class Stack
+	{
+		private ArrayList stack;
+
+		private Stack()
+		{
+			stack = new ArrayList ();
+		}
+
+		//void Push(object obj)
+			//store the given object on top of the stack (need to use object type)
+			// take in scenerior that null is passed in - do not store it
+
+		//object Pop()
+			// removes the object at the top of the stack and returns it
+		    //take into account that you should throw a InvalidOperationExpection if its called on a empty stack
+
+		//void Clear()
+			//removes all objects from the stack
+
+	}
+}
+
